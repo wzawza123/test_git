fb8d43c583b2b924856a5cfba68afa804609f8d4
wzawza123 <936956317@qq.com>
2023-01-17 16:28:20 +0800

    update the hello world

---
+++ new_program.cpp
@@ -0,0 +1,12 @@
+/*
+ * @Description: 
+ * @Date: 2023-01-17 16:23:38
+ * @LastEditTime: 2023-01-17 16:28:04
+ */
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Hello World";
+    return 0;
+}
